9726466f437c34a88aadbb604e640921e4495e9d
Duc Trung Le <leductrungxf@gmail.com>
2023-12-10 01:55:00 +0100

    Update path

--- jupytercad_salome/__init__.py
+++ jupytercad_salome/__init__.py
@@ -13,7 +13,7 @@ from .handlers import setup_handlers
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "jupytercad-salome"
+        "dest": "@jupytercad/jupytercad-salome"
     }]
 
 
