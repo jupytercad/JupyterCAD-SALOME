0d57a37a4e6dd036a7f326b1251ea8efc6a4a00e
Duc Trung Le <leductrungxf@gmail.com>
2023-12-10 10:57:06 +0100

    Add precommit

--- jupytercad_salome/__init__.py
+++ jupytercad_salome/__init__.py
@@ -5,22 +5,18 @@ except ImportError:
     # in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
+
     warnings.warn("Importing 'jupytercad_salome' outside a proper installation.")
     __version__ = "dev"
 from .handlers import setup_handlers
 
 
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": "@jupytercad/jupytercad-salome"
-    }]
+    return [{"src": "labextension", "dest": "@jupytercad/jupytercad-salome"}]
 
 
 def _jupyter_server_extension_points():
-    return [{
-        "module": "jupytercad_salome"
-    }]
+    return [{"module": "jupytercad_salome"}]
 
 
 def _load_jupyter_server_extension(server_app):
--- jupytercad_salome/handlers.py
+++ jupytercad_salome/handlers.py
@@ -4,15 +4,16 @@ from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
+
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
     def get(self):
-        self.finish(json.dumps({
-            "data": "This is /jupytercad-salome/get-example endpoint!"
-        }))
+        self.finish(
+            json.dumps({"data": "This is /jupytercad-salome/get-example endpoint!"})
+        )
 
 
 def setup_handlers(web_app):
